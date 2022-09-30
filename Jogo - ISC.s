ca52e125bdf46755d86bbd3d54bd21fdd7a42d03
Neve0 <nevenarcisista@gmail.com>
2022-09-30 17:22:41 -0300

    Adicionado mecânica de tiros
    Pressione 'k' para atirar.

--- Jogo - ISC.s
+++ Jogo - ISC.s
@@ -2,6 +2,7 @@
 CHAR_POS: 	.half 16, 32
 OLD_CHAR_POS:	.half 16, 32
 CORD_SHOT:	.half 0, 0
+OLD_CORD_SHOT:	.half 0, 0
 .include "sprites/charUP.data"
 .include "sprites/charD.data"
 .include "sprites/charR.data"
@@ -31,7 +32,21 @@ SETUP: 		la,  a0, mapa4
 		li a3, 1
 		call PRINT
 		
-GAME_LOOP:	call KEY2
+		mv s3, zero
+		
+GAME_LOOP:	li t0, 1
+		li t1, 2
+		li t2, 3
+		li t3, 4
+
+		beq s3, t0, SHOT_DIR
+		beq s3, t1, SHOT_ESQ
+		beq s3, t2, SHOT_UP
+		beq s3, t3, SHOT_DOWN
+		
+		call KEY2
+		
+
 		
 		xori s0, s0, 1
 		
@@ -41,6 +56,7 @@ GAME_LOOP:	call KEY2
 		lh a2, 2(t0)
 		mv a3, s0
 		call PRINT
+		
 		
 		li t0, 0xFF200604
 		sw s0, 0(t0)
@@ -54,6 +70,8 @@ GAME_LOOP:	call KEY2
 		mv a3, s0
 		xori a3, a3, 1
 		call PRINT
+		
+		
 		
 		j GAME_LOOP
 		
@@ -77,12 +95,13 @@ KEY2:		li t1,0xFF200000
 		
 		li t0, 'k'
 		beq t2, t0, DIR_SHOT
+		
+		
 	
-FIM:		ret
+FIM:	
+		ret
 
-CHAR_ESQ: 	
-
-		la t0, CHAR_POS
+CHAR_ESQ:	la t0, CHAR_POS
 		la t1, OLD_CHAR_POS
 		lw t2, 0(t0)
 		sw t2, 0(t1)
@@ -187,32 +206,258 @@ CHAR_DOWN: 	la t0, CHAR_POS
 		mv s2, a0
 		ret
 		
-DIR_SHOT:	#la t0, charL
-		#beq s2, t0, SHOT_ESQ
+DIR_SHOT:	la t1, CHAR_POS  #### SET COORDENADA INICIAL TIRO
+		la t6, CORD_SHOT
+		lh t2, 0(t1)
+		sh t2, 0(t6)
+		lh t2, 2(t1)
+		sh t2, 2(t6)
+		################
+		
+		# CODIGOS DIR_SHOT
+		# s3 = 0 TIRO NAO ATIVADO
+		# s3 = 1 DIREITA
+		# s3 = 2 ESQUERDA
+		# s3 = 3 CIMA
+		# s4 = 4 BAIXO
+		############### 
+		
+		la t0, charL
+		beq s2, t0, SHOT_ESQ
 		
 		la t0, charR
 		beq s2, t0, SHOT_DIR
 		
-		#la t0, charUP
-		#beq s2, t0, SHOT_UP
 		
-		#la t0, charD
-		#beq s2, t0, SHOT_DOWN
+		la t0, charUP
+		beq s2, t0, SHOT_UP
 		
-SHOT_DIR:	la t0, CHAR_POS
-		lh t1, 0(t0) # t1 = X
-		lh t2, 2(t0) # t2 = Y
+		la t0, charD
+		beq s2, t0, SHOT_DOWN
+		
+		j GAME_LOOP
+		
+SHOT_DIR:       li s3, 1  # instancia SHOT
+		la t3, CORD_SHOT
+		la t4, OLD_CORD_SHOT
+		lh t1, 0(t3) # t1 = X
+		sh t1, 0(t4)
+		lh t2, 2(t3) # t2 = Y
+		sh t2, 2(t4)
 		addi t1, t1, 16
 		
-		la t3, CORD_SHOT
 		
+	
+		sh t1, 0(t3) 
+		sh t2, 2(t3) 
+		
+		## CHECAGEM COLISAO TIRO
+		la a0, mapa4
+		addi a0, a0, 8
+		add a0, a0, t1
+		li t3, 320
+		mul t3, t3, t2
+		add a0, a0, t3
+		lb  t4, 0(a0)
+		bne t4, zero, FIM_SHOT
+		#################
+		
+		la t3, CORD_SHOT
 		la a0, shotHor
-		sh t1, 0(t3)
-		lh a1, 0(t3) #a1 = X
-		sh t2, 2(t0) 
-		sh a2, 2(t3) #a2 =  Y
+		lh a1, 0(t3)
+		lh a2, 2(t3)
+		li a3, 0
 		call PRINT
+		
+		li a3, 1
+		call PRINT
+		
+		
+		la t1, CHAR_POS
+		lh t2, 0(t1)
+		la t3, OLD_CORD_SHOT
+		lh t4, 0(t3)
+		beq t2, t4, SHOT_DIR
+		la a0, tileP
+		lh a1, 0(t3)
+		lh a2, 2(t3)
+		li a3, 0
+		call PRINT
+		li a3, 1
+		call PRINT
+		
 		j GAME_LOOP
+		
+SHOT_ESQ:       li s3, 2  # instancia SHOT
+		la t3, CORD_SHOT
+		la t4, OLD_CORD_SHOT
+		lh t1, 0(t3) # t1 = X
+		sh t1, 0(t4)
+		lh t2, 2(t3) # t2 = Y
+		sh t2, 2(t4)
+		addi t1, t1, -16
+		
+		
+	
+		sh t1, 0(t3) 
+		sh t2, 2(t3) 
+		
+		## CHECAGEM COLISAO TIRO
+		la a0, mapa4
+		addi a0, a0, 8
+		add a0, a0, t1
+		li t3, 320
+		mul t3, t3, t2
+		add a0, a0, t3
+		lb  t4, 0(a0)
+		bne t4, zero, FIM_SHOT
+		#################
+		
+		la t3, CORD_SHOT
+		la a0, shotHor
+		lh a1, 0(t3)
+		lh a2, 2(t3)
+		li a3, 0
+		call PRINT
+		
+		li a3, 1
+		call PRINT
+		
+		
+		la t1, CHAR_POS
+		lh t2, 0(t1)
+		la t3, OLD_CORD_SHOT
+		lh t4, 0(t3)
+		beq t2, t4, SHOT_ESQ
+		la a0, tileP
+		lh a1, 0(t3)
+		lh a2, 2(t3)
+		li a3, 0
+		call PRINT
+		li a3, 1
+		call PRINT
+		
+		j GAME_LOOP
+		
+SHOT_UP:        li s3, 3  # instancia SHOT
+		la t3, CORD_SHOT
+		la t4, OLD_CORD_SHOT
+		lh t1, 0(t3) # t1 = X
+		sh t1, 0(t4)
+		lh t2, 2(t3) # t2 = Y
+		sh t2, 2(t4)
+		addi t2, t2, -16
+		
+		
+	
+		sh t1, 0(t3) 
+		sh t2, 2(t3) 
+		
+		## CHECAGEM COLISAO TIRO
+		la a0, mapa4
+		addi a0, a0, 8
+		add a0, a0, t1
+		li t3, 320
+		mul t3, t3, t2
+		add a0, a0, t3
+		lb  t4, 0(a0)
+		bne t4, zero, FIM_SHOT
+		#################
+		
+		la t3, CORD_SHOT
+		la a0, shotVer
+		lh a1, 0(t3)
+		lh a2, 2(t3)
+		li a3, 0
+		call PRINT
+		
+		li a3, 1
+		call PRINT
+		
+		
+		la t1, CHAR_POS
+		lh t2, 2(t1)
+		la t3, OLD_CORD_SHOT
+		lh t4, 2(t3)
+		beq t2, t4, SHOT_UP
+		la a0, tileP
+		lh a1, 0(t3)
+		lh a2, 2(t3)
+		li a3, 0
+		call PRINT
+		li a3, 1
+		call PRINT
+		
+		j GAME_LOOP
+
+SHOT_DOWN:      li s3, 4  # instancia SHOT
+		la t3, CORD_SHOT
+		la t4, OLD_CORD_SHOT
+		lh t1, 0(t3) # t1 = X
+		sh t1, 0(t4)
+		lh t2, 2(t3) # t2 = Y
+		sh t2, 2(t4)
+		addi t2, t2, 16
+		
+		
+	
+		sh t1, 0(t3) 
+		sh t2, 2(t3) 
+		
+		## CHECAGEM COLISAO TIRO
+		la a0, mapa4
+		addi a0, a0, 8
+		add a0, a0, t1
+		li t3, 320
+		mul t3, t3, t2
+		add a0, a0, t3
+		lb  t4, 0(a0)
+		bne t4, zero, FIM_SHOT
+		#################
+		
+		la t3, CORD_SHOT
+		la a0, shotVer
+		lh a1, 0(t3)
+		lh a2, 2(t3)
+		li a3, 0
+		call PRINT
+		
+		li a3, 1
+		call PRINT
+		
+		
+		la t1, CHAR_POS
+		lh t2, 2(t1)
+		la t3, OLD_CORD_SHOT
+		lh t4, 2(t3)
+		beq t2, t4, SHOT_DOWN
+		la a0, tileP
+		lh a1, 0(t3)
+		lh a2, 2(t3)
+		li a3, 0
+		call PRINT
+		li a3, 1
+		call PRINT
+		
+		j GAME_LOOP		
+FIM_SHOT: mv s3, zero
+	  la t0, OLD_CORD_SHOT
+	  la a0, tileP
+	  lh a1, 0(t0)
+	  lh a2, 2(t0)
+	  li a3, 0
+	  call PRINT
+	  li a3, 1
+	  call PRINT
+	  j GAME_LOOP
+	  
+
+	  
+		
+		
+		  
+		 
+		
 		
 		
 
